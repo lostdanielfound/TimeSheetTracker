7d70a81f1172e835a310ee9230f4f79f07eecc1b
Daniel Guzman <daniellostsilver2000@gmail.com>
2023-01-17 08:10:44 +0000

    Changed syntax of main

--- main.c
+++ main.c
@@ -12,46 +12,21 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <ncurses.h>
 #include <time.h>
 #include "Clock.h"
 #include "Timefun.h" 
+#include "cursorSelection.h"
 
 int main(void) {
 
-	printf("\n\n\t-------------------------------\n"); 
-	printf("\t------Time Logging System------\n"); 
-	printf("\t-------------------------------\n"); 
-
-	int c; /* ASCII character input from STDIN */
+	int choice;
+	char* options[4] = {"Create New User", "Clock-In User", "Clock-Out User", "Show TimeSheet"};
 	
-	while(1) 
+	while(1)
 	{
-		printf("\n\t1) Clock In\n"); 
-		printf("\t2) Clock Out\n"); 
-		printf("\t3) Review\n"); 
-		printf("\t4) Add\n"); 
-		printf("\t5) Delete\n"); 
-		printf("\n\t6) Exit\n\t: ");
-
-		do { c = getchar(); /* I should implement select() here... */ } while (isdigit(c) == 0); /* Keep lopping until user input's a number */  
-
-		time_t current_time = time(NULL); /* Gather current system time */
-
-		int status_code = 0; 
-		switch (c) {
-			case '1': status_code = Clock_In(current_time); break; 
-			case '2': status_code = Clock_Out(current_time); break;
-			case '3': status_code = Time_Review(current_time); break;
-			case '4': status_code = Time_Add(current_time); break;
-			case '5': status_code = Time_Delete(current_time); break;
-			case '6': return 0; 
-			default: return 0; 
-		}
-
-		printf("\n\t-------------------------------\n");
-
-		/* If error occured, then report on stdio 
-		( idk how to report on stderr ) */
-		if (status_code < 0) { return status_code; }
+		choice = cursorSelection(options, 4, "Time Logging System");
 	}
-} 
+
+	return 0;
+}
