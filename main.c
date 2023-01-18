612c443fecca3bd0e1d2829893948803247faf5f
Daniel Guzman <daniellostsilver2000@gmail.com>
2023-01-18 21:35:43 +0000

    Started implementing sep of conserns program flow

--- main.c
+++ main.c
@@ -27,35 +27,44 @@
 #include "Time_Functions.h" 
 #include "cursorSelection.h"
 
+void CtlC_switch(char** usernames) { free(usernames); exit(0); }
+
 int main(void) {
 
-	int choice;
-	char* current_clockin_user = NULL; 
+	//Variables that are created at the beginning of Runtime
+	//NOTE, any variables that are considered LARGE in C standard should 
+	//be created here instead of being created within another function
+	//to prevent overusage of heap memory.
+	//(Bruh don't use so much memory, this is C)
+	//-----------------------------------------------------------------------------------	
+	int choice = 0;
+	char** usernames = (char**)calloc(MAX_CLOCKED_IN, USERNAME_MAX); //array of strings that hold the current the names of the clocked-in users
 	char* default_options[3] = {"Create New User", "Clock-In User", "Show TimeSheet"};
 	char* LoggedIn_options[3] = {"Create New User", "Clock-Out User", "Show TimeSheet"};
-	
-	while(1)
+	//-----------------------------------------------------------------------------------	
+
+
+	//Signal defines will be placed here	
+	//-----------------------------------------------------------------------------------	
+	signal(SIGINT, CtlC_switch);
+	//-----------------------------------------------------------------------------------	
+
+	while(1) 	
 	{
-		if (current_clockin_user != NULL) //THIS MAKES LITERALLY NO SENCE, BRUH i NEED TO CHANGE THIS 
+		//First want to check which users are currently Clocked-In
+		ListClockedIn(usernames);
+		switch(choice) 
 		{
-			choice = cursorSelection(LoggedIn_options, 3, "Time Logging System", current_clockin_user);
-		}
-		choice = cursorSelection(default_options, 3, "Time Logging System");
-
-		swtich(choice)
-		{
-			case 0: //Create new User
-
-				break; 
-			case 1: //Clock-in User
-
-				break; 
-			case 2: //Clock-out User
-				break; 
-			case 3: //Show Timesheet
-				break;
-			default: 
-				break;
+		case 0: //Create new User
+			break; 
+		case 1: //Clock-in User
+			break; 
+		case 2: //Clock-out User
+			break; 
+		case 3: //Show Timesheet
+			break;
+		default: 
+			break;
 		}
 	}
 
