614c6d8cf46612e25fcc7c3f2815b5adb9bcb526
Daniel Guzman <daniellostsilver2000@gmail.com>
2023-01-20 23:53:47 +0000

    created defined header files for choice interface

---
+++ Choice_Functions.c
@@ -0,0 +1,17 @@
+#include "Choice_Functions.h"
+#include <ncurses.h>
+
+/**
+ * @brief 
+ * Shows user a dialog on creating the new user that they want to add to the database, 
+ * Uses ncurses to provide a clean User interface 
+ * @return 0 if User creation was a success, on error will return a negative error code, 
+ * refer to program_macros for list of error codes 
+ */
+int Create_User_dialog() {
+
+}
+
+int Clock_In_user();
+int Clock_Out_user();
+int Show_Timesheet();
---
+++ Choice_Functions.h
@@ -0,0 +1,10 @@
+#ifndef CHOICE_FUN_H
+#define CHOICE_FUN_H
+#include "Clock.h"
+
+int Create_User_dialog();
+int Clock_In_user();
+int Clock_Out_user();
+int Show_Timesheet();
+
+#endif
